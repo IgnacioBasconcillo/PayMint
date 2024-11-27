e7a0de1a5b5cb17afabdb841dc8cf25bfb74b8db
Nachorly <nachobjl.99@gmail.com>
2024-11-27 19:03:46 +0100

    Modular Project Structure

---
+++ paymint-api/boot/src/main/java/com/paymint/paymint_api/PayMintApiApplication.java
@@ -0,0 +1,13 @@
+package com.paymint.paymint_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMintApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMintApiApplication.class, args);
+	}
+
+}
